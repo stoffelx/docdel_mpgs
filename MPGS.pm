e38c5b96fa46a012e4e299bb0a41b81de051ab4e
Christoph Hümmer <huemmer@mpdl.mpg.de>
2023-08-04 12:54:58 +0200

    implement minor improvements

--- MPGS.pm
+++ MPGS.pm
@@ -1,11 +1,10 @@
-# Version: $Id: MPGS.pm, v. 0.1, by CH 25.07.2023
+# Version: $Id: MPGS.pm, v. 0.2, by CH 04.08.2023
 package Parsers::TargetParser::DOCUMENT_DELIVERY::MPGS;
 use strict;
 use base qw(Parsers::TargetParser);
 use URI;
 
 sub getDocumentDelivery {
-#       my ($this, $ctx_obj) = @_;
         my ($this) = @_;
 
         my $svc         = $this->{svc};
@@ -14,14 +13,14 @@ sub getDocumentDelivery {
         my $genre       = $ctx_obj->get('rft.genre') || '';
         my $issn        = $ctx_obj->get('rft.issn') || '';
 
-        my $url1        = $svc->parse_param('bookurl');
-        my $url2        = $svc->parse_param('articleurl');
+        my $url1        = $svc->parse_param('bookurl') || '';
+        my $url2        = $svc->parse_param('articleurl') || '';
 
 
         my $url = "";
 
 
-        if ($issn ne '' || ($genre =~ /^(journal|issue|article|proceeding|conference|preprint|dissertation)$/)) {
+        if ($issn || ($genre =~ /^(journal|issue|article|proceeding|conference|preprint|dissertation)$/)) {
             $url = $url2;
             }
             else {
@@ -29,11 +28,11 @@ sub getDocumentDelivery {
             }
 
 
-        my $uri = "";
+        my $uri;
         my %query = ();
 
 
-        my $uri = URI->new($url);
+        $uri = URI->new($url);
         $uri->query_form(%query);
         return ($uri);
 }
